2ccc25e0763dc4ce87d9d1b7d660b2b2d36ac63d
Boas Demeson <47591907+boazdemeson@users.noreply.github.com>
2026-02-25 18:19:23 +0700

    Add files via upload

---
+++ test_add_remove_elements/simple_click.js
@@ -0,0 +1,27 @@
+const {By,until,Builder} = require("selenium-webdriver");
+require("chromedriver");
+
+async function addButton(){
+ 
+        const driver = await new Builder().forBrowser('chrome').build();
+        
+            try {
+            await driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+
+            await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//div[@id="content"]/h3'))),10000);
+
+            const button = await driver.findElement(By.xpath('//div[@id="content"]/div[@class="example"]/button'));
+            await button.click();
+
+            const element = await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//div[@id="elements"]/button[@class="added-manually"]'))),10000 );
+            //console.log('Element is visible!', await element.getText());
+            await element.click();
+            }
+
+            finally {
+            await driver.quit();
+            }
+ 
+}
+
+addButton()
